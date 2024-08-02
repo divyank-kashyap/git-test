5d6bba1648f12ab5bfc9580d0c95c9ced61638fe
divyank-kashyap <hyperxtron380@gmail.com>
2024-08-02 18:15:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const questions = [{
+    title: "question 1",
+    description: "Add two numbers"
+}, {
+    title: "question 2",
+    description: "Median of Two sorted Arrays"
+}]
+
+
+
+app.get("/", (req, res)=> {
+    res.send(questions);
+    // console.log(questions);
+})
+
+app.listen(port, function (req, res ) {
+     console.log(`App running on port ${port}`)
+})
